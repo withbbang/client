776104a26d661e9ae0e3db1fa71f5d1f63c58cb5
unknown <withbbang@gmail.com>
2023-03-08 18:57:19 +0900

    add jsencrypt

--- src/screens/index/IndexCT.tsx
+++ src/screens/index/IndexCT.tsx
@@ -1,19 +1,37 @@
 import React, { useEffect, useState } from 'react';
+import { JSEncrypt } from 'jsencrypt';
 import IndexPT from './IndexPT';
 
 const IndexCT = () => {
   useEffect(() => {
-    fetch('test')
+    fetch('server/sign')
       .then((res) => {
         return res.json();
       })
-      .then((result) => console.log(result));
+      .then((result) => {
+        console.log('result.key: ', result.key);
+        const encrypt = new JSEncrypt();
+        const plain = 'Hellow World';
 
-    fetch('server/b')
-      .then((res) => {
-        return res.json();
-      })
-      .then((result) => console.log(result));
+        encrypt.setPublicKey(result.key);
+        let encrypted;
+        try {
+          encrypted = encrypt.encrypt(plain);
+        } catch (e) {
+          console.log(e);
+        }
+        console.log('encrypted: ', encrypted);
+
+        fetch('server/sign', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({ data: encrypted })
+        })
+          .then((response) => response.json())
+          .then((data) => console.log(data));
+      });
   }, []);
 
   return <IndexPT />;
