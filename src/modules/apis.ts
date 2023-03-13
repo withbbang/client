bedec169384725083ba22edc98ccab2531eea056
김영선 <withbbang@gmail.com>
2023-03-13 22:43:25 +0900

    500 error 처리

--- src/modules/apis.ts
+++ src/modules/apis.ts
@@ -10,7 +10,9 @@ function getAPI(url: string): Promise<any> {
       }
     })
       .then((response) => {
-        if (response.status === 404) {
+        if (response.status === 500) {
+          reject(response);
+        } else if (response.status === 404) {
         } else if (response.status === 200) {
           return response.json();
         } else {
@@ -44,7 +46,9 @@ function postAPI(url: string, { payload }: { payload: Object }): Promise<any> {
       body: JSON.stringify(payload)
     })
       .then((response) => {
-        if (response.status === 404) {
+        if (response.status === 500) {
+          reject(response);
+        } else if (response.status === 404) {
         } else if (response.status === 200) {
           return response.json();
         } else {
@@ -78,7 +82,9 @@ function putAPI(url: string, { payload }: { payload: Object }): Promise<any> {
       body: JSON.stringify(payload)
     })
       .then((response) => {
-        if (response.status === 404) {
+        if (response.status === 500) {
+          reject(response);
+        } else if (response.status === 404) {
         } else if (response.status === 200) {
           return response.json();
         } else {
@@ -115,7 +121,9 @@ function deleteAPI(
       body: JSON.stringify(payload)
     })
       .then((response) => {
-        if (response.status === 404) {
+        if (response.status === 500) {
+          reject(response);
+        } else if (response.status === 404) {
         } else if (response.status === 200) {
           return response.json();
         } else {
