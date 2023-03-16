da226947247fb0943da376536451103adcd4331b
unknown <withbbang@gmail.com>
2023-03-16 19:09:20 +0900

    쿠키함수 설명 주석 추가

--- src/modules/cookie.ts
+++ src/modules/cookie.ts
@@ -2,6 +2,11 @@ import { Cookies } from 'react-cookie';
 
 const cookies = new Cookies();
 
+/**
+ * 쿠키 읽기
+ * @param {string} name 쿠키 key
+ * @returns {string} 쿠키 value
+ */
 export function handleGetCookie(name: string): string {
   return cookies.get(name);
 }
