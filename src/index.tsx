50911ae92c85224ef62c75e6998478350855d01e
unknown <withbbang@gmail.com>
2023-03-23 11:40:18 +0900

    redux persist 주석

--- src/index.tsx
+++ src/index.tsx
@@ -14,6 +14,8 @@ ReactDOM.createRoot(rootNode!).render(
   //   <App />
   // </React.StrictMode>
   <Provider store={store}>
+    {/* loading: 로딩 과정을 보여줄 컴포넌트 */}
+    {/* persistor: 로컬 스토리지에 저장할 store (결국 여기선 persistStore가 반환한 값) */}
     <PersistGate loading={null} persistor={persistStore(store)}>
       <App />
     </PersistGate>
