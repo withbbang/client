ee97be67b4450988176d4be69d724dca78aaed38
unknown <withbbang@gmail.com>
2023-04-17 10:56:04 +0900

    기능 팝업 dnd 적용

--- src/components/functionPopup/FunctionPopup.tsx
+++ src/components/functionPopup/FunctionPopup.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PropState } from 'middlewares/configureReducer';
 import { CommonState } from 'middlewares/reduxTookits/commonSlice';
 import { connect } from 'react-redux';
@@ -22,6 +22,55 @@ const FunctionPopup = ({
   children,
   onClose
 }: typeFunctionPopup): JSX.Element => {
+  const [isDragging, setIsDragging] = useState<boolean>(false);
+  const [currentX, setCurrentX] = useState<number | undefined>();
+  const [currentY, setCurrentY] = useState<number | undefined>();
+  const [initialX, setInitialX] = useState<number | undefined>();
+  const [initialY, setInitialY] = useState<number | undefined>();
+  const [xOffset, setXOffset] = useState<number>(0);
+  const [yOffset, setYOffset] = useState<number>(0);
+
+  const handleSetTranslate = (
+    xPos: number,
+    yPos: number,
+    el: HTMLElement | null
+  ) => {
+    if (el) {
+      el.style.transform = 'translate3d(' + xPos + 'px, ' + yPos + 'px, 0)';
+    }
+  };
+
+  const handleDragStart = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setInitialX(e.clientX - xOffset);
+    setInitialY(e.clientY - yOffset);
+
+    if (e.target === document.getElementById('item')) {
+      setIsDragging(true);
+    }
+  };
+
+  const handleDragEnd = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    setInitialX(currentX);
+    setInitialX(currentY);
+    setIsDragging(false);
+  };
+
+  const handleDrag = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (isDragging) {
+      e.preventDefault();
+
+      initialX && setCurrentX(e.clientX - initialX);
+      initialY && setCurrentY(e.clientY - initialY);
+
+      currentX && setXOffset(currentX);
+      currentY && setYOffset(currentY);
+
+      currentX &&
+        currentY &&
+        handleSetTranslate(currentX, currentY, document.getElementById('item'));
+    }
+  };
+
   return (
     <>
       {isActive ? (
@@ -31,16 +80,12 @@ const FunctionPopup = ({
               ? [styles.background, styles.night].join(' ')
               : styles.background
           }
+          id={'container'}
+          onMouseDown={handleDragStart}
+          onMouseUp={handleDragEnd}
+          onMouseMove={handleDrag}
         >
-          <div className={styles.modal_body}>
-            <div className={styles.dnd} draggable>
-              <SVG
-                type="dnd"
-                width="25px"
-                height="25px"
-                fill={isNight ? '#fff' : '#000'}
-              />
-            </div>
+          <div className={styles.modal_body} id={'item'} draggable>
             <div className={styles.close} onClick={() => onClose()}>
               <SVG
                 type="close"
